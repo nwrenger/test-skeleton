eb0924257d45ae48ed3942efce9b0ddd67363e1d
Nils Wrenger <nils@wrenger.net>
2025-09-24 11:31:12 +0200

    :sparkles: Added working modal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,22 +9,43 @@
 </script>
 
 <Modal
+	triggerBase="btn preset-tonal"
+	contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl max-w-screen-sm"
+	backdropClasses="backdrop-blur-sm"
+>
+	{#snippet trigger()}Open Modal (Working){/snippet}
+	{#snippet content()}
+		<header class="flex justify-between">
+			<h2 class="h2">Working Modal</h2>
+		</header>
+		<article>
+			<p class="opacity-60">
+				When closing this modal, an animation will play. Everything works as expected!
+			</p>
+		</article>
+		<footer class="flex justify-end gap-4">
+			<button type="button" class="btn preset-tonal" onclick={modalClose}>Cancel</button>
+			<button type="button" class="btn preset-filled" onclick={modalClose}>Confirm</button>
+		</footer>
+	{/snippet}
+</Modal>
+
+<Modal
 	open={openState}
 	onOpenChange={(e) => (openState = e.open)}
 	triggerBase="btn preset-tonal"
 	contentBase="card bg-surface-100-900 p-4 space-y-4 shadow-xl max-w-screen-sm"
 	backdropClasses="backdrop-blur-sm"
 >
-	{#snippet trigger()}Open Modal{/snippet}
+	{#snippet trigger()}Open Modal (Broken){/snippet}
 	{#snippet content()}
 		<header class="flex justify-between">
-			<h2 class="h2">Modal Example</h2>
+			<h2 class="h2">Broken Modal</h2>
 		</header>
 		<article>
 			<p class="opacity-60">
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, ab adipisci. Libero cumque
-				sunt quis error veritatis amet, expedita voluptatem. Quos repudiandae consequuntur
-				voluptatem et dicta quas, reprehenderit velit excepturi?
+				When closing this modal, no animation will play. A closing animation is expected, so
+				something is wrong!
 			</p>
 		</article>
 		<footer class="flex justify-end gap-4">
